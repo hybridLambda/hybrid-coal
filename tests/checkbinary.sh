c84f12334fe07f2455d9ee90082f5c1fb221814d
Joe Zhu <sha.joe.zhu@gmail.com>
2015-08-20 14:28:02 +0100

    include check total probability script

--- tests/checkbinary.sh
+++ tests/checkbinary.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 function test_hybrid-coal {
   echo -n " hybrid-coal $@ "
-  for i in `seq 1 1`; do
-    echo -n "."
+  echo -n "."
 
-    if [ -f hybrid-coal_dbg ]; then
-      # Test using hybrid-coal self-checks
-      ./hybrid-coal_dbg $@ > /dev/null
-      if [ $? -ne 0 ]; then
-        echo ""
-        echo "Executing \"./hybrid-coal_dbg $@ \" failed."
-        echo "Debug Call: make -mj2 hybrid-coal_dbg && ./hybrid-coal_dbg $@ 2>&1 | less"
-        exit 1
-      fi
-    fi
-
-    ## Test for memory leaks
-    valgrind --error-exitcode=1 --leak-check=full -q ./hybrid-coal $@ > /dev/null
+  if [ -f hybrid-coal_dbg ]; then
+    # Test using hybrid-coal self-checks
+    ./hybrid-coal_dbg $@ > /dev/null
     if [ $? -ne 0 ]; then
       echo ""
-      echo "Valgrind check of \"./hybrid-coal $@ \" failed."
+      echo "Executing \"./hybrid-coal_dbg $@ \" failed."
+      echo "Debug Call: make -mj2 hybrid-coal_dbg && ./hybrid-coal_dbg $@ 2>&1 | less"
       exit 1
     fi
+  fi
 
-  done
+  ## Test for memory leaks
+  valgrind --error-exitcode=1 --leak-check=full -q ./hybrid-coal $@ > /dev/null
+  if [ $? -ne 0 ]; then
+    echo ""
+    echo "Valgrind check of \"./hybrid-coal $@ \" failed."
+    exit 1
+  fi
+
   echo " done."
 }
 
