6b81850f651b2c0de9b381e603af0ad7fc6b9f25
Joe Zhu <sha.joe.zhu@gmail.com>
2015-08-25 20:32:07 +0100

    include thesis tree

---
+++ tests/thesis/scripts.sh
@@ -0,0 +1,8 @@
+../../hybrid-coal -sp WIII0abc -o WIII0abc
+../../hybrid-coal -sp WIII0aabbcc -o WIII0aabbcc 
+../../hybrid-coal -sp WIII0aabbbc -o WIII0aabbbc
+
+../../hybrid-coal -sp WIII1abc -o WIII1abc
+../../hybrid-coal -sp WIII1aabbcc -o WIII1aabbcc 
+../../hybrid-coal -sp WIII1aabbbc -o WIII1aabbbc
+
